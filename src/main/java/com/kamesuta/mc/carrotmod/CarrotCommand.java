6521c9b54fc836071c8041c9ac162f03991b4881
Kamesuta <kamesuta@gmail.com>
2016-06-11 15:06:31 +0900

    Command Update

--- src/main/java/com/kamesuta/mc/carrotmod/CarrotCommand.java
+++ src/main/java/com/kamesuta/mc/carrotmod/CarrotCommand.java
@@ -4,23 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 import cpw.mods.fml.common.FMLCommonHandler;
-import net.minecraft.command.ICommand;
+import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.ChatStyle;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IChatComponent;
 
-public class CarrotCommand implements ICommand {
+public class CarrotCommand extends CommandBase {
 	protected final CarrotBubu bubu;
 
-	private List<String> aliases;
+	private final List<String> aliases;
 
 	public CarrotCommand() {
-		bubu = new CarrotBubu();
-		FMLCommonHandler.instance().bus().register(bubu);
+		this.bubu = new CarrotBubu();
+		FMLCommonHandler.instance().bus().register(this.bubu);
 
 		this.aliases = new ArrayList<String>();
 		this.aliases.add("carrot");
@@ -33,7 +36,7 @@ public class CarrotCommand implements ICommand {
 	}
 
 	@Override
-	public String getCommandUsage(ICommandSender icommandsender) {
+	public String getCommandUsage(final ICommandSender icommandsender) {
 		return "carrot <command>";
 	}
 
@@ -43,95 +46,108 @@ public class CarrotCommand implements ICommand {
 	}
 
 	@Override
-	public void processCommand(ICommandSender icommandsender, String[] astring) {
+	public void processCommand(final ICommandSender icommandsender, final String[] astring) {
 		if (astring.length >= 1 && "bubu".equals(astring[0]))
 		{
-			int count = parseIntDefault(astring[astring.length-1], -1);
+			final int count = parseIntDefault(astring[astring.length-1], -1);
 
 			if (astring.length >= 2 && !(parseIntDefault(astring[1], -1) >= 0)) {
-				boolean hasPermission = (icommandsender instanceof EntityPlayer) ?
+				final boolean hasPermission = (icommandsender instanceof EntityPlayer) ?
 						MinecraftServer.getServer().getConfigurationManager().func_152596_g(((EntityPlayer)icommandsender).getGameProfile()) :
-						true;
-				if(hasPermission) {
-					EntityPlayer reciever = MinecraftServer.getServer().getEntityWorld().getPlayerEntityByName(astring[1]);
-					if ("-all".equals(astring[1])) {
-						sendServerChat(new ChatComponentText(
-								icommandsender.getCommandSenderName() + " had all players BUBUed!")
+							true;
+						if(hasPermission) {
+							//					EntityPlayer reciever = MinecraftServer.getServer().getEntityWorld().getPlayerEntityByName(astring[1]);
+							final EntityPlayerMP reciever = getPlayer(icommandsender, astring[1]);
+							if ("-all".equals(astring[1])) {
+								sendServerChat(new ChatComponentText(
+										icommandsender.getCommandSenderName() + " had all players BUBUed!")
 										.setChatStyle(new ChatStyle().setColor(EnumChatFormatting.GOLD)));
-						List<?> players = MinecraftServer.getServer().getEntityWorld().playerEntities;
-						for (Object playerobj : players)
-						{
-							EntityPlayer play = (EntityPlayer) playerobj;
-							bubu.addPlayer(play, count);
+								final List<?> players = MinecraftServer.getServer().getEntityWorld().playerEntities;
+								for (final Object playerobj : players)
+								{
+									final EntityPlayer play = (EntityPlayer) playerobj;
+									this.bubu.addPlayer(play, count);
+								}
+							} else if ("-other".equals(astring[1])) {
+								sendServerChat(new ChatComponentText(
+										icommandsender.getCommandSenderName() + " had all players BUBUed!")
+										.setChatStyle(new ChatStyle().setColor(EnumChatFormatting.GOLD)));
+								final List<?> players = MinecraftServer.getServer().getEntityWorld().playerEntities;
+								for (final Object playerobj : players)
+								{
+									final EntityPlayer play = (EntityPlayer) playerobj;
+									if (!icommandsender.equals(play)) this.bubu.addPlayer(play, count);
+								}
+							} else if (reciever != null) {
+								sendServerChat(new ChatComponentText(
+										icommandsender.getCommandSenderName() + " had " + reciever.getCommandSenderName() + " BUBUed!")
+										.setChatStyle(new ChatStyle().setColor(EnumChatFormatting.GOLD)));
+								this.bubu.addPlayer(reciever, count);
+							} else {
+								sendClientChat(icommandsender, new ChatComponentText("Invailed name " + astring[1] + ".").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.RED)));
+							}
+						} else {
+							sendClientChat(icommandsender, new ChatComponentText("You don't have permission to use [bubu.other]").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.RED)));
 						}
-					} else if ("-other".equals(astring[1])) {
-						sendServerChat(new ChatComponentText(
-								icommandsender.getCommandSenderName() + " had all players BUBUed!")
-										.setChatStyle(new ChatStyle().setColor(EnumChatFormatting.GOLD)));
-						List<?> players = MinecraftServer.getServer().getEntityWorld().playerEntities;
-						for (Object playerobj : players)
-						{
-							EntityPlayer play = (EntityPlayer) playerobj;
-							if (!icommandsender.equals(play)) bubu.addPlayer(play, count);
-						}
-					} else if (reciever != null) {
-						sendServerChat(new ChatComponentText(
-								icommandsender.getCommandSenderName() + " had " + reciever.getCommandSenderName() + " BUBUed!")
-										.setChatStyle(new ChatStyle().setColor(EnumChatFormatting.GOLD)));
-						bubu.addPlayer(reciever, count);
-					} else {
-						sendClientChat(icommandsender, new ChatComponentText("Invailed name " + astring[1] + ".").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.RED)));
-					}
-				} else {
-					sendClientChat(icommandsender, new ChatComponentText("You don't have permission to use [bubu.other]").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.RED)));
-				}
 			} else {
 				if (icommandsender instanceof EntityPlayer) {
-					bubu.addPlayer(((EntityPlayer)icommandsender), count);
+					this.bubu.addPlayer(((EntityPlayer)icommandsender), count);
 				} else {
 					sendClientChat(icommandsender, new ChatComponentText("Usage /h bubu <[name]|-all|-other>").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.RED)));
 				}
 			}
+		} else if (astring.length >= 1 && "me".equals(astring[0])) {
+			IChatComponent c0 = icommandsender.func_145748_c_();
+			final IChatComponent ichatcomponent = func_147176_a(icommandsender, astring, 1, icommandsender.canCommandSenderUseCommand(1, "carrot"));
+			if (icommandsender instanceof EntityPlayer) {
+				final EntityPlayer player = (EntityPlayer)icommandsender;
+				final ItemStack item = player.getHeldItem();
+				if (item != null) {
+					c0 = c0.appendSibling(item.func_151000_E());
+				}
+			}
+			sendServerChat(new ChatComponentTranslation("chat.type.emote", new Object[] {c0, ichatcomponent}));
 		} else {
 			sendClientChat(icommandsender, new ChatComponentText("/carrot <-bubu>"));
 		}
 	}
 
 	@Override
-	public boolean canCommandSenderUseCommand(ICommandSender icommandsender) {
-		return true;
+	public int getRequiredPermissionLevel()
+	{
+		return 0;
 	}
 
 	@Override
-	public List<String> addTabCompletionOptions(ICommandSender icommandsender, String[] astring) {
+	public List<String> addTabCompletionOptions(final ICommandSender icommandsender, final String[] astring) {
 		return null;
 	}
 
 	@Override
-	public boolean isUsernameIndex(String[] astring, int i) {
+	public boolean isUsernameIndex(final String[] astring, final int i) {
 		return false;
 	}
 
 	@Override
-	public int compareTo(Object o) {
+	public int compareTo(final Object o) {
 		return 0;
 	}
 
-	public static int parseIntDefault(String s, int d)
+	public static int parseIntDefault(final String s, final int d)
 	{
 		int count = d;
 		try {
 			count = Integer.parseInt(s);
-		} catch (NumberFormatException e) {}
+		} catch (final NumberFormatException e) {}
 		return count;
 	}
 
-	public static void sendServerChat(IChatComponent chat)
+	public static void sendServerChat(final IChatComponent chat)
 	{
 		MinecraftServer.getServer().getConfigurationManager().sendChatMsg(chat);
 	}
 
-	public static void sendClientChat(ICommandSender sender, IChatComponent chat)
+	public static void sendClientChat(final ICommandSender sender, final IChatComponent chat)
 	{
 		sender.addChatMessage(chat);
 	}
